1b1b90047564a73db9f770376eb00257e00cc77b
ManuelAP <manuel.aullo@cybercom.com>
2018-06-25 11:23:37 +0300

    KTO-39: Koulutuksen sivupalkki selaintuki
    Puuttuva style koodirivi css arkistosta
    *oppija-urls.js - revertoitu

--- src/main/app/src/oppija-urls.js
+++ src/main/app/src/oppija-urls.js
@@ -1,3 +1,4 @@
+
 const production = {
     "konfo-backend.base-url" : "/",
     "konfo-backend.search.koulutukset" : "/konfo-backend/search/koulutukset",
@@ -8,25 +9,16 @@ const production = {
     "kartta.base-url" : "https://hkp.maanmittauslaitos.fi",
     "kartta.publish-url" : "https://hkp.maanmittauslaitos.fi/hkp/published/$1/277da693-ae10-4508-bc5a-d6ced2056fd0"
 };
+
 const development = {
-    "konfo-backend.base-url" : "https://hahtuvaopintopolku.fi",
-    "konfo-backend.search.koulutukset" : "https://hahtuvaopintopolku.fi/konfo-backend/search/koulutukset",
-    "konfo-backend.search.oppilaitokset" : "https://hahtuvaopintopolku.fi/konfo-backend/search/oppilaitokset",
-    "konfo-backend.koulutus" : "https://hahtuvaopintopolku.fi/konfo-backend/koulutus/",
-    "konfo-backend.oppilaitos" : "https://hahtuvaopintopolku.fi/konfo-backend/oppilaitos/",
-    "konfo-backend.palaute" : "https://hahtuvaopintopolku.fi/konfo-backend/palaute",
+    "konfo-backend.base-url" : "http://localhost:3006",
+    "konfo-backend.search.koulutukset" : "http://localhost:3006/konfo-backend/search/koulutukset",
+    "konfo-backend.search.oppilaitokset" : "http://localhost:3006/konfo-backend/search/oppilaitokset",
+    "konfo-backend.koulutus" : "http://localhost:3006/konfo-backend/koulutus/",
+    "konfo-backend.oppilaitos" : "http://localhost:3006/konfo-backend/oppilaitos/",
+    "konfo-backend.palaute" : "http://localhost:3006/konfo-backend/palaute",
     "kartta.base-url" : "https://hkp.maanmittauslaitos.fi",
     "kartta.publish-url" : "https://hkp.maanmittauslaitos.fi/hkp/published/$1/277da693-ae10-4508-bc5a-d6ced2056fd0"
 };
-// const development = {
-//     "konfo-backend.base-url" : "http://localhost:3006",
-//     "konfo-backend.search.koulutukset" : "http://localhost:3006/konfo-backend/search/koulutukset",
-//     "konfo-backend.search.oppilaitokset" : "http://localhost:3006/konfo-backend/search/oppilaitokset",
-//     "konfo-backend.koulutus" : "http://localhost:3006/konfo-backend/koulutus/",
-//     "konfo-backend.oppilaitos" : "http://localhost:3006/konfo-backend/oppilaitos/",
-//     "konfo-backend.palaute" : "http://localhost:3006/konfo-backend/palaute",
-//     "kartta.base-url" : "https://hkp.maanmittauslaitos.fi",
-//     "kartta.publish-url" : "https://hkp.maanmittauslaitos.fi/hkp/published/$1/277da693-ae10-4508-bc5a-d6ced2056fd0"
-// };
 
 export {production, development}
