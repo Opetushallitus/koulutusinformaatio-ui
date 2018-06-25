6e85f07cf4236235505213337a2e42303f7a426d
Joona Haavisto <joona.haavisto@gofore.com>
2018-06-25 18:45:16 +0300

    Tyylimuutoksia ja hakulaatikko

--- src/main/app/src/components/common/SidebarDropdown.js
+++ src/main/app/src/components/common/SidebarDropdown.js
@@ -4,9 +4,7 @@ class SidebarDropdown extends Component {
     render() {
         return (
             <li className={"dropdown" + (this.props.selected ? " open" : "")}>
-                <a className="dropdown-toggle" onClick={() => this.props.selectDropdown()}>{this.props.name}
-                    <span className="caret"/>
-                </a>
+                <a className="dropdown-toggle" onClick={() => this.props.selectDropdown()}>{this.props.name}</a>
                 <ul className="dropdown-menu" role="menu">
                     {this.props.links.map((i) =>
                         <li key={i.name}>
