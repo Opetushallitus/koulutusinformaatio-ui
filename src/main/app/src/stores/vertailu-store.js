a5bff0c97cde9cd426d7724c600f5cd832df1809
Joona Haavisto <joona.haavisto@gofore.com>
2018-06-26 18:18:46 +0300

    Vertailusivu koulutuksille

--- src/main/app/src/stores/vertailu-store.js
+++ src/main/app/src/stores/vertailu-store.js
@@ -1,24 +1,95 @@
-import { observable, action } from "mobx"
+import { observable, action, computed, toJS } from "mobx";
+import { Localizer as l } from "../tools/Utils";
+
 
 class VertailuStore {
-    @observable vertailuList = [];
+    constructor(hakuStore, restStore) {
+        this.hakuStore = hakuStore;
+        this.rest = restStore;
+    }
+
+    @observable vertailuListKoulutus = [];
+    @observable vertailuListOppilaitos = [];
+    @observable vertailuResults = [];
+    @observable sizeKoulutus = 0;
+    @observable sizeOppilaitos = 0;
+
+    @computed get oids() {
+        return this.vertailuList.map((i) => i.oid);
+    }
+
+    @computed get size() {
+        return this.hakuStore.toggleKoulutus ? this.sizeKoulutus : this.sizeOppilaitos;
+    }
+
+    @computed get vertailuList() {
+        return this.hakuStore.toggleKoulutus ? this.vertailuListKoulutus : this.vertailuListOppilaitos;
+    }
+
+    @computed get createVertailuLink() {
+        return "/vertailu?oids=" + this.oids.join(',') + "&haku=" + encodeURIComponent(this.hakuStore.createHakuUrl) + "&lng=" + l.getLanguage();
+    }
 
     @action
     isOidSelected = (oid) => {
         return this.vertailuList.findIndex((item) => item.oid === oid) !== -1;
     };
+
     @action
-    selectKoulutus = (oid, nimi, link) => {
-        if (!this.isOidSelected(oid)) {
-            this.vertailuList.push({oid: oid, nimi: nimi, link: link});
+    selectItem = (oid, nimi, link) => {
+        if (this.hakuStore.toggleKoulutus) {
+            if (!this.isOidSelected(oid)) {
+                this.vertailuListKoulutus.push({oid: oid, nimi: nimi, link: link});
+            }
+            this.sizeKoulutus = this.vertailuListKoulutus.length;
+        } else {
+            if (!this.isOidSelected(oid)) {
+                this.vertailuListOppilaitos.push({oid: oid, nimi: nimi, link: link});
+            }
+            this.sizeOppilaitos = this.vertailuListOppilaitos.length;
         }
     };
+
     @action
-    removeKoulutus = (oid) => {
-        if (this.isOidSelected(oid)) {
-            this.vertailuList = this.vertailuList.filter((i) => i.oid !== oid);
+    removeItem = (oid) => {
+        if (this.hakuStore.toggleKoulutus) {
+            if (this.isOidSelected(oid)) {
+                this.vertailuListKoulutus = this.vertailuListKoulutus.filter((i) => i.oid !== oid);
+            }
+            this.sizeKoulutus = this.vertailuListKoulutus.length;
+        } else {
+            if (this.isOidSelected(oid)) {
+                this.vertailuListOppilaitos = this.vertailuListOppilaitos.filter((i) => i.oid !== oid);
+            }
+            this.sizeOppilaitos = this.vertailuListOppilaitos.length;
         }
-    }
+    };
+
+    @action
+    clearItems = () => {
+        if (this.hakuStore.toggleKoulutus) {
+            this.vertailuListKoulutus = [];
+            this.sizeKoulutus = 0;
+        } else {
+            this.vertailuListOppilaitos = [];
+            this.sizeOppilaitos = 0;
+        }
+    };
+
+    @action
+    loadVertailuItems = (oids) => {
+        this.vertailuResults = [];
+        const cbKoulutus = (results) => {this.vertailuResults = results.map((res, i) => {
+                return (res.result.koulutus) ? toJS(res.result.koulutus[oids[i]]) : undefined;
+            }); console.log(results)
+        };
+
+        if (this.hakuStore.toggleKoulutus) {
+            this.rest.search(oids.map((i) => this.rest.getKoulutusPromise(i)), cbKoulutus)
+        } else {
+            this.rest.search(oids.map((i) => this.rest.getOppilaitosPromise(i)), () => (""))
+        }
+    };
 }
 
 export default VertailuStore;
