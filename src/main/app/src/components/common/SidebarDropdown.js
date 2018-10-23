bddfbee1d7ea8c706dc23220e7e1e628fb64cfd9
ManuelAP <manuel.aullo@cybercom.com>
2018-10-23 14:24:37 +0300

    Vertailu mobile
    Vertailu sivu toimii mobilissa
    Valikko transitiot
    Lisätty uus breakpointi

--- src/main/app/src/components/common/SidebarDropdown.js
+++ src/main/app/src/components/common/SidebarDropdown.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import {Link, withRouter} from 'react-router-dom';
 
 class SidebarDropdown extends Component {
+    state = {
+            collapsing: "collapsed"
+        };
+
+    collapseMenu(){
+        this.setState({
+            collapsing: "collapsing"
+        });
+        setTimeout(() => {
+            this.setState({
+                collapsing: "collapsed"
+            });
+        },175);
+    }
+
     render() {
         return (
-            <li className={"dropdown" + (this.props.selected ? " open" : "")}>
-                <a className="dropdown-toggle" onClick={() => this.props.selectDropdown()}>{this.props.name}</a>
-                <ul className="dropdown-menu" role="menu">
+            <li className={"dropdown " + (this.props.selected ? "current-page"  : this.state.collapsing)}>
+                <a className="dropdown-toggle" onClick={ () => {this.props.selectDropdown(); this.collapseMenu()}}>{this.props.name}</a>
+                <ul className={"dropdown-menu " + (this.props.selected ? "expanded" : this.state.collapsing)} role="menu">
                     {this.props.links.map((i) =>
                         <li key={i.name} className={i.link === this.props.location.pathname ? "current-page" : ""}>
                             <Link to={i.link}>{i.name}</Link>
--- src/main/app/src/components/vertailu/Vertailu.js
+++ src/main/app/src/components/vertailu/Vertailu.js
@@ -5,6 +5,7 @@ import { translate } from 'react-i18next';
 import { inject, observer } from 'mobx-react';
 import { Localizer as l } from "../../tools/Utils";
 import qs from 'querystring';
+import Media from 'react-media';
 import '../../assets/styles/components/_vertailu.scss';
 
 @translate()
@@ -40,7 +41,7 @@ class Vertailu extends Component {
     renderComparedItems() {
         return this.props.vertailuStore.vertailuList.map((item, i) => {
             return (
-                <div className="col-md-3">
+                <div className="col-sm-12 col-md-3">
                     <ul className={"compare-list " + this.colors[i]} key={item.oid}>
                         <li className={"compared-items"} >
                             <div className="compare-box">
@@ -93,6 +94,200 @@ class Vertailu extends Component {
             </React.Fragment>
         )
     }
+    renderComparedItemsMobile(){
+
+        return this.props.vertailuStore.vertailuList.map((item, i) => {
+            return ( 
+                <div className="compare-items">
+                        <div className={"compare-list " + this.colors[i]} key={item.oid}>
+                            <div className={"compared-items"} >
+                                <div className="compare-box">
+                                    <Link to={item.link} className="title">
+                                        {item.organisaatio ? item.organisaatio.nimi + ":" : ""}{l.localize(item.searchData, item.nimi)}
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                </div>            
+            )
+        });
+    }
+    renderItemsContentMobile(){
+        const t = this.props.t;
+        return(
+            <div>
+                <div className="row compare-info">
+                    <div className="col-12">
+                        <p>{t("vertailu.koulutus-alkaa")}</p>
+                    </div>
+                    {
+                        this.props.vertailuStore.vertailuList.map((item, x) => {
+                            return(
+                                <div className="col-12 info-col d-flex" item={item} itemNum={x}>
+                                    <div className={"col-11 " + this.colors[x]}>
+                                        <p>{(l.localize(item.koulutuksenAlkamiskausi) + " " + item.koulutuksenAlkamisvuosi)}</p>
+                                    </div>
+                                </div>
+                            );
+                            
+                        })
+                    }
+                </div>
+                <div className="row compare-info">
+                    <div className="col-12">
+                        <p>{t("vertailu.opetuskieli")}</p>
+                    </div>
+                    {
+                        this.props.vertailuStore.vertailuList.map((item, x) => {
+                            return(
+                                <div className="col-12 info-col d-flex">
+                                    <div className={"col-11 " + this.colors[x]}>
+                                        <p>{(item.opetuskielis && item.opetuskielis.map((k) => l.localize(k)).join(', '))}</p>
+                                    </div>
+                                </div>
+                            );
+                            
+                        })
+                    }
+                </div>
+                <div className="row compare-info">
+                    <div className="col-12">
+                        <p>{t("vertailu.koulutuksen-laajuus")}</p>
+                    </div>
+                    {
+                        this.props.vertailuStore.vertailuList.map((item, x) => {
+                            return(
+                                <div className="col-12 info-col d-flex">
+                                    <div className={"col-11 " + this.colors[x]} >
+                                        <p>{(l.localize(item.opintojenLaajuusarvo, "", "fi") + " " + l.localize(item.opintojenLaajuusyksikko))}</p>
+                                    </div>
+                                </div>
+                            );
+                            
+                        })
+                    }
+                </div>
+                <div className="row compare-info">
+                    <div className="col-12">
+                        <p>{t("vertailu.suunniteltu-kesto")}</p>
+                    </div>
+                    {
+                        this.props.vertailuStore.vertailuList.map((item, x) => {
+                            return(
+                                <div className="col-12 info-col d-flex">
+                                    <div className={"col-11 " + this.colors[x]} >
+                                        <p>{(item.suunniteltuKestoArvo + " " + l.localize(item.suunniteltuKestoTyyppi))}</p>
+                                    </div>
+                                </div>
+                            );
+                            
+                        })
+                    }
+                </div>
+                <div className="row compare-info">
+                    <div className="col-12">
+                        <p>{t("vertailu.osaamisalat")}</p>
+                    </div>
+                    {
+                        this.props.vertailuStore.vertailuList.map((item, x) => {
+                            return(
+                                <div className="col-12 info-col d-flex">
+                                    <div className={"col-11 " + this.colors[x]} >
+                                        <p>{l.localize(item.opintoala)} </p>
+                                    </div>
+                                </div>
+                            );
+                            
+                        })
+                    }
+                </div>
+                <div className="row compare-info">
+                    <div className="col-12">
+                        <p>{t("vertailu.opetusaika")}</p>
+                    </div>
+                    {
+                        this.props.vertailuStore.vertailuList.map((item, x) => {
+                            return(
+                                <div className="col-12 info-col d-flex">
+                                    <div className={"col-11 " + this.colors[x]} >
+                                        <p>{(item.opetusAikas && item.opetusAikas.map((i) => l.localize(i)).join(', '))}</p>
+                                    </div>
+                                </div>
+                            );
+                            
+                        })
+                    }
+                </div>
+                <div className="row compare-info">
+                    <div className="col-12">
+                        <p>{t("vertailu.opetustapa")}</p>
+                    </div>
+                    {
+                        this.props.vertailuStore.vertailuList.map((item, x) => {
+                            return(
+                                <div className="col-12 info-col d-flex">
+                                    <div className={"col-11 " + this.colors[x]} >
+                                        <p>{(item.opetusPaikkas && item.opetusPaikkas.map((i) => l.localize(i)).join(', ')) }</p>
+                                    </div>
+                                </div>
+                            );
+                            
+                        })
+                    }
+                </div>
+                <div className="row compare-info">
+                    <div className="col-12">
+                        <p>{t("vertailu.opiskelumuoto")}</p>
+                    </div>
+                    {
+                        this.props.vertailuStore.vertailuList.map((item, x) => {
+                            return(
+                                <div className="col-12 info-col d-flex">
+                                    <div className={"col-11 " + this.colors[x]} >
+                                        <p>{(item.opetusmuodos && item.opetusmuodos.map((i) => l.localize(i)).join(', '))}</p>
+                                    </div>
+                                </div>
+                            );
+                            
+                        })
+                    }
+                </div>
+                <div className="row compare-info">
+                    <div className="col-12">
+                        <p>{t("vertailu.pohjakoulutus")}</p>
+                    </div>
+                    {
+                        this.props.vertailuStore.vertailuList.map((item, x) => {
+                            return(
+                                <div className="col-12 info-col d-flex">
+                                    <div className={"col-11 " + this.colors[x]} >
+                                        <p>{(l.localize(item.pohjakoulutusvaatimukset) || l.localize(item.pohjakoulutusvaatimus))}</p>
+                                    </div>
+                                </div>
+                            );
+                            
+                        })
+                    }
+                </div>
+                <div className="row compare-info">
+                    <div className="col-12">
+                        <p>{t("vertailu.maksullinen")}</p>
+                    </div>
+                    {
+                        this.props.vertailuStore.vertailuList.map((item, x) => {
+                            return(
+                                <div className="col-12 info-col d-flex">
+                                    <div className={"col-11 " + this.colors[x]} >
+                                        <p>{(item.opintojenMaksullisuus ? this.props.t("vertailu.maksullinen") : this.props.t("vertailu.maksuton"))}</p>
+                                    </div>
+                                </div>
+                            ); 
+                        })
+                    }
+                </div>
+            </div> 
+        )
+    }
     render() {
         const t = this.props.t;
         return (
@@ -103,36 +298,52 @@ class Vertailu extends Component {
                             <h1>
                                 <Link to={this.state.hakuUrl} className="header" >
                                 <span className="icon-ic-back-button"></span>
-                                    <span className="light-font">{t("vertailu.takaisin-edelliselle-sivulle")}</span>
+                                    <span className="light-font">{t("vertailu.takaisin-edelliselle-sivulle")}<br /><strong>{t("vertailu.vertailuun-ottamasi-kohteet")}</strong></span>
                                   {/*t("vertailu.vertailuun-ottamasi-kohteet")*/}
                                 </Link>
                             </h1>
                         </div>
                     </div>
                 </div>
-                <div className="container">
-                    <div className="compare-page row">
-                        <div className="col-md-3">
-                            <ul className="compare-list subject-titles">
-                                <li className="compared-items">
-                                    <div className="compare-box no-content"></div>
-                                </li>
-                                <li className="compare-list-item"><p>{t("vertailu.koulutus-alkaa")}</p></li>
-                                <li className="compare-list-item"><p>{t("vertailu.opetuskieli")}</p></li>
-                                <li className="compare-list-item"><p>{t("vertailu.koulutuksen-laajuus")}</p></li>
-                                <li className="compare-list-item"><p>{t("vertailu.suunniteltu-kesto")}</p></li>
-                                <li className="compare-list-item"><p>{t("vertailu.osaamisalat")}</p></li>
-                                <li className="compare-list-item"><p>{t("vertailu.opetusaika")}</p></li>
-                                <li className="compare-list-item"><p>{t("vertailu.opetustapa")}</p></li>
-                                <li className="compare-list-item"><p>{t("vertailu.opiskelumuoto")}</p></li>
-                                <li className="compare-list-item"><p>{t("vertailu.pohjakoulutus")}</p></li>
-                                <li className="compare-list-item"><p>{t("vertailu.maksullinen")}</p></li>
-                            </ul>
-                        </div>
-                            
-                        {this.renderComparedItems()}
-                        </div>
-                </div>
+            <Media query="(max-width: 992px)">
+                        {
+                            matches => matches ? (
+                                <div className="container compare-mobile">
+                                    {this.renderComparedItemsMobile()}
+                                    {this.renderItemsContentMobile()}
+                                </div>
+                            ) 
+                                : 
+                            (
+                                <div className="container">
+                                    <div className="compare-page row">
+                                        <div className="col-md-3">
+                                            <ul className="compare-list subject-titles">
+                                                <li className="compared-items">
+                                                    <div className="compare-box no-content"></div>
+                                                </li>
+                                                <li className="compare-list-item"><p>{t("vertailu.koulutus-alkaa")}</p></li>
+                                                <li className="compare-list-item"><p>{t("vertailu.opetuskieli")}</p></li>
+                                                <li className="compare-list-item"><p>{t("vertailu.koulutuksen-laajuus")}</p></li>
+                                                <li className="compare-list-item"><p>{t("vertailu.suunniteltu-kesto")}</p></li>
+                                                <li className="compare-list-item"><p>{t("vertailu.osaamisalat")}</p></li>
+                                                <li className="compare-list-item"><p>{t("vertailu.opetusaika")}</p></li>
+                                                <li className="compare-list-item"><p>{t("vertailu.opetustapa")}</p></li>
+                                                <li className="compare-list-item"><p>{t("vertailu.opiskelumuoto")}</p></li>
+                                                <li className="compare-list-item"><p>{t("vertailu.pohjakoulutus")}</p></li>
+                                                <li className="compare-list-item"><p>{t("vertailu.maksullinen")}</p></li>
+                                            </ul>
+                                        </div>
+                                            
+                                        {this.renderComparedItems()}
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </Media>    
+
+                
+                
                 
                 <Hakunavigaatio vertailu={true}/>
             </React.Fragment>
