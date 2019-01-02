3705b2b9948252afe46d038496d3bed6dc9a47c3
ManuelAP <manuel.aullo@cybercom.com>
2019-01-02 16:17:01 +0200

    Hakurajaimet näppäimistön eventit
    HakuRajainValinta toimii näppäimistön avulla

--- src/main/app/src/components/haku/Hakurajainvalinta.js
+++ src/main/app/src/components/haku/Hakurajainvalinta.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import '../../assets/styles/components/_hakurajainvalinta.scss';
 class Hakurajainvalinta extends Component {
+    constructor(props) {
+        super(props);
+      this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.props = nextProps;
+    }
+
+    handleKeyPress(e) {
+       this.props.handleChange();
+    }
 
     render() {
         return (
             <React.Fragment>
-                <div className="checkbox" tabIndex="0">
+                <div className="checkbox" tabIndex="0"  onKeyPress={this.handleKeyPress}>
                     <input role="menuitem" id={this.props.text} type="checkbox" checked={this.props.checked} onChange={this.props.handleChange} />
                     <label htmlFor={this.props.text}>{this.props.text}</label>
                     {this.props.color && <span className={"cr ch-" + this.props.color}> </span>}
