28846929e3a0df15d30f5d1d5922ab065f7eb774
Mikko Tuppurainen <mikkotuppurainen@HEL-5CG7234N54.global.ad>
2019-05-08 12:50:18 +0300

    oppilaitoksen yhteystiedot toteutus-sivulle

--- src/main/app/src/components/toteutus/Ammatillinen.js
+++ src/main/app/src/components/toteutus/Ammatillinen.js
@@ -20,7 +20,7 @@ class Ammatillinen extends Component {
         const fields = {};
         fields.left = this.parseInfoBoxFieldsLeft();
         fields.otsikkoLeft = t('koulutus.tiedot');
-        fields.hakuajat = this.props.koulutus.hakuajatFromBackend;
+        fields.hakuajat = this.props.toteutus.hakuajatFromBackend;
         fields.otsikkoRight = t('koulutus.hae-koulutukseen');
         return fields;
     }
@@ -30,34 +30,34 @@ class Ammatillinen extends Component {
         const fields = [];
         // laajuus, kesto, maksullinen, tutkintonimike
 
-        fields.push([t('toteutus.alkaa'), (this.props.koulutus.metadata.opetus.alkamiskausi && this.props.koulutus.metadata.opetus.alkamiskausi.nimi) ? l.localize(this.props.koulutus.metadata.opetus.alkamiskausi.nimi) +" "+ this.props.koulutus.metadata.opetus.alkamisvuosi : '-']);
-        fields.push([t('toteutus.opetuskieli'), (this.props.koulutus.metadata.opetus.opetuskieli && this.props.koulutus.metadata.opetus.opetuskieli["0"]) ? l.localize(this.props.koulutus.metadata.opetus.opetuskieli["0"].nimi) : '-']);
-        fields.push([t('toteutus.opetusaika'), (this.props.koulutus.metadata.opetus.opetusaika && this.props.koulutus.metadata.opetus.opetusaika.nimi) ? l.localize(this.props.koulutus.metadata.opetus.opetusaika.nimi) : '-']);
-        fields.push([t('toteutus.opetustapa'), (this.props.koulutus.metadata.opetus.opetustapa && this.props.koulutus.metadata.opetus.opetustapa["0"]) ? l.localize(this.props.koulutus.metadata.opetus.opetustapa["0"].nimi) : '-']);
+        fields.push([t('toteutus.alkaa'), (this.props.toteutus.metadata.opetus.alkamiskausi && this.props.toteutus.metadata.opetus.alkamiskausi.nimi) ? l.localize(this.props.toteutus.metadata.opetus.alkamiskausi.nimi) +" "+ this.props.toteutus.metadata.opetus.alkamisvuosi : '-']);
+        fields.push([t('toteutus.opetuskieli'), (this.props.toteutus.metadata.opetus.opetuskieli && this.props.toteutus.metadata.opetus.opetuskieli["0"]) ? l.localize(this.props.toteutus.metadata.opetus.opetuskieli["0"].nimi) : '-']);
+        fields.push([t('toteutus.opetusaika'), (this.props.toteutus.metadata.opetus.opetusaika && this.props.toteutus.metadata.opetus.opetusaika.nimi) ? l.localize(this.props.toteutus.metadata.opetus.opetusaika.nimi) : '-']);
+        fields.push([t('toteutus.opetustapa'), (this.props.toteutus.metadata.opetus.opetustapa && this.props.toteutus.metadata.opetus.opetustapa["0"]) ? l.localize(this.props.toteutus.metadata.opetus.opetustapa["0"].nimi) : '-']);
         fields.push([t('toteutus.laajuus'), '-']); //koulutuksesta?
         fields.push([t('toteutus.kesto'), '-']); //koulutuksesta?
-        fields.push([t('toteutus.maksullinen'), this.props.koulutus.metadata.opetus.onkoMaksullinen ? t('kyllä') : t('ei')]);
-        fields.push([t('toteutus.lukuvuosimaksu'), this.props.koulutus.metadata.opetus.lukuvuosimaksu.maksu ? (this.props.koulutus.metadata.opetus.lukuvuosimaksu.maksu + 'eur') : '0 eur']); //vika objekti tuntematon?
+        fields.push([t('toteutus.maksullinen'), this.props.toteutus.metadata.opetus.onkoMaksullinen ? t('kyllä') : t('ei')]);
+        fields.push([t('toteutus.lukuvuosimaksu'), this.props.toteutus.metadata.opetus.lukuvuosimaksu.maksu ? (this.props.toteutus.metadata.opetus.lukuvuosimaksu.maksu + 'eur') : '0 eur']); //vika objekti tuntematon?
 
         return fields;
     }
 
     render() {
-        const kuvaus = this.props.koulutus.metadata.kuvaus;
-        console.log(kuvaus);
-        const osaamisalat = this.props.koulutus.metadata.osaamisalat;
+        const kuvaus = this.props.toteutus.metadata.kuvaus;
+        console.log(this);
+        const osaamisalat = this.props.toteutus.metadata.osaamisalat;
         const jatkoopinnot = "tba";
         const {t} = this.props;
         return (
             <React.Fragment>
                 <div className="col-12 col-md-12 col-lg-8 col-xl-9 left-column">
-                    <ToteutusHeader komoOid={this.props.koulutus.komoOid}
-                                    nimi={this.props.koulutus.nimi}
-                                    organisaatio={this.props.koulutus.organisaatio.nimi}/>
+                    <ToteutusHeader komoOid={this.props.toteutus.komoOid}
+                                    nimi={this.props.toteutus.nimi}
+                                    organisaatio={this.props.toteutus.organisaatio.nimi}/>
                     <ToteutusInfoBox fields={this.parseInfoBoxFieldsTwoSided()}/>
 
                     {kuvaus &&
-                        <SlideDropdown kuvaus={true} teksti={l.localize(this.props.koulutus.metadata.kuvaus)} title={t('toteutus.kuvaus')} />
+                        <SlideDropdown kuvaus={true} teksti={l.localize(this.props.toteutus.metadata.kuvaus)} title={t('toteutus.kuvaus')} />
                     }
 
                     {osaamisalat && 
@@ -68,7 +68,7 @@ class Ammatillinen extends Component {
                     }
 
                 </div>
-                <ToteutusSidebar organisaatio={this.props.organisaatio} koulutus={this.props.koulutus} educationType={this.educationType}/>
+                <ToteutusSidebar organisaatio={this.props.organisaatio} koulutus={this.props.toteutus} educationType={this.educationType}/>
             </React.Fragment>
         );
     }
--- src/main/app/src/components/toteutus/Korkeakoulu.js
+++ src/main/app/src/components/toteutus/Korkeakoulu.js
@@ -17,10 +17,10 @@ class Korkeakoulu extends Component {
 
     parseAineListaus() {
         const {t} = this.props;
-        if(this.props.koulutus.oppiaineet.length > 0) {
-            return this.props.koulutus.oppiaineet.map(o => <li key={o.oppiaine ? o.oppiaine : ''} className="osaamisalat_list_item">{o.oppiaine ? o.oppiaine : t("tuntematon")}</li>);
+        if(this.props.toteutus.oppiaineet.length > 0) {
+            return this.props.toteutus.oppiaineet.map(o => <li key={o.oppiaine ? o.oppiaine : ''} className="osaamisalat_list_item">{o.oppiaine ? o.oppiaine : t("tuntematon")}</li>);
         } else {
-            return this.props.koulutus.aihees.map(a => <li key={a.uri} className="osaamisalat_list_item">{l.localize(a.nimi)}</li>);
+            return this.props.toteutus.aihees.map(a => <li key={a.uri} className="osaamisalat_list_item">{l.localize(a.nimi)}</li>);
         }
     }
 
@@ -29,7 +29,7 @@ class Korkeakoulu extends Component {
         const fields = {};
         fields.left = this.parseInfoBoxFieldsLeft();
         fields.otsikkoLeft = t('koulutus.tiedot');
-        fields.hakuajat = this.props.koulutus.hakuajatFromBackend;
+        fields.hakuajat = this.props.toteutus.hakuajatFromBackend;
         fields.otsikkoRight = t('koulutus.hae-koulutukseen');
         return fields;
     }
@@ -39,15 +39,15 @@ class Korkeakoulu extends Component {
         const fields = [];
         // laajuus, kesto, maksullinen, tutkintonimike
 
-        fields.push([t('toteutus.alkaa'), (this.props.koulutus.metadata.opetus.alkamiskausi && this.props.koulutus.metadata.opetus.alkamiskausi.nimi) ? l.localize(this.props.koulutus.metadata.opetus.alkamiskausi.nimi) +" "+ this.props.koulutus.metadata.opetus.alkamisvuosi : '-']);
-        fields.push([t('toteutus.opetuskieli'), (this.props.koulutus.metadata.opetus.opetuskieli && this.props.koulutus.metadata.opetus.opetuskieli["0"]) ? l.localize(this.props.koulutus.metadata.opetus.opetuskieli["0"].nimi) : '-']);
-        fields.push([t('toteutus.opetusaika'), (this.props.koulutus.metadata.opetus.opetusaika && this.props.koulutus.metadata.opetus.opetusaika.nimi) ? l.localize(this.props.koulutus.metadata.opetus.opetusaika.nimi) : '-']);
-        fields.push([t('toteutus.opetustapa'), (this.props.koulutus.metadata.opetus.opetustapa && this.props.koulutus.metadata.opetus.opetustapa["0"]) ? l.localize(this.props.koulutus.metadata.opetus.opetustapa["0"].nimi) : '-']);
+        fields.push([t('toteutus.alkaa'), (this.props.toteutus.metadata.opetus.alkamiskausi && this.props.toteutus.metadata.opetus.alkamiskausi.nimi) ? l.localize(this.props.toteutus.metadata.opetus.alkamiskausi.nimi) +" "+ this.props.toteutus.metadata.opetus.alkamisvuosi : '-']);
+        fields.push([t('toteutus.opetuskieli'), (this.props.toteutus.metadata.opetus.opetuskieli && this.props.toteutus.metadata.opetus.opetuskieli["0"]) ? l.localize(this.props.toteutus.metadata.opetus.opetuskieli["0"].nimi) : '-']);
+        fields.push([t('toteutus.opetusaika'), (this.props.toteutus.metadata.opetus.opetusaika && this.props.toteutus.metadata.opetus.opetusaika.nimi) ? l.localize(this.props.toteutus.metadata.opetus.opetusaika.nimi) : '-']);
+        fields.push([t('toteutus.opetustapa'), (this.props.toteutus.metadata.opetus.opetustapa && this.props.toteutus.metadata.opetus.opetustapa["0"]) ? l.localize(this.props.toteutus.metadata.opetus.opetustapa["0"].nimi) : '-']);
         fields.push([t('toteutus.laajuus'), '-']); //koulutuksesta?
         fields.push([t('toteutus.kesto'), '-']); //koulutuksesta?
-        fields.push([t('toteutus.maksullinen'), this.props.koulutus.metadata.opetus.onkoMaksullinen ? t('kyllä') : t('ei')]);
-        fields.push([t('toteutus.lukuvuosimaksu'), this.props.koulutus.metadata.opetus.lukuvuosimaksu.maksu ? (this.props.koulutus.metadata.opetus.lukuvuosimaksu.maksu + 'eur') : '0 eur']); //vika objekti tuntematon?
-        fields.push([t('toteutus.stipendit'), this.props.koulutus.metadata.opetus.stipendinKuvaus ? t('Kyllä') : t('ei')]);
+        fields.push([t('toteutus.maksullinen'), this.props.toteutus.metadata.opetus.onkoMaksullinen ? t('kyllä') : t('ei')]);
+        fields.push([t('toteutus.lukuvuosimaksu'), this.props.toteutus.metadata.opetus.lukuvuosimaksu.maksu ? (this.props.toteutus.metadata.opetus.lukuvuosimaksu.maksu + 'eur') : '0 eur']); //vika objekti tuntematon?
+        fields.push([t('toteutus.stipendit'), this.props.toteutus.metadata.opetus.stipendinKuvaus ? t('Kyllä') : t('ei')]);
 
 
         return fields;
@@ -55,22 +55,22 @@ class Korkeakoulu extends Component {
 
     render() {
         const {t} = this.props;
-        const osaamisalat = this.props.koulutus.metadata.alemmanKorkeakoulututkinnonOsaamisalat.concat(this.props.koulutus.metadata.ylemmanKorkeakoulututkinnonOsaamisalat);
+        const osaamisalat = this.props.toteutus.metadata.alemmanKorkeakoulututkinnonOsaamisalat.concat(this.props.toteutus.metadata.ylemmanKorkeakoulututkinnonOsaamisalat);
         return (
             <React.Fragment>
                 <div className="col-12 col-md-12 col-lg-8 col-xl-9 left-column">
-                    <ToteutusHeader komoOid={this.props.koulutus.komoOid}
+                    <ToteutusHeader komoOid={this.props.toteutus.komoOid}
                                     organisaatioOid={this.props.organisaatio.oid}
-                                    nimi={this.props.koulutus.metadata.nimi}
-                                    organisaatio={this.props.koulutus.organisaatio.nimi}/>
+                                    nimi={this.props.toteutus.metadata.nimi}
+                                    organisaatio={this.props.toteutus.organisaatio.nimi}/>
                     <ToteutusInfoBox fields={this.parseInfoBoxFieldsTwoSided()}/>
-                        <SlideDropdown kuvaus={true} teksti={l.localize(this.props.koulutus.metadata.kuvaus)} title={t('toteutus.kuvaus')} />        
+                        <SlideDropdown kuvaus={true} teksti={l.localize(this.props.toteutus.metadata.kuvaus)} title={t('toteutus.kuvaus')} />        
                         <SlideDropdown title={t('toteutus.pääaineet')} osaamisalat={true} osaamisalatlist={osaamisalat.length > 0 ? osaamisalat : false}/>
 
 
                         <SlideDropdown text={true} title={t('toteutus.jatko-opintomahdollisuudet')}/>
                 </div>
-                <ToteutusSidebar organisaatio={this.props.organisaatio} koulutus={this.props.koulutus} educationType={this.educationType}/>
+                <ToteutusSidebar organisaatio={this.props.organisaatio} toteutus={this.props.toteutus} educationType={this.educationType}/>
             </React.Fragment>   
         );
     }
