ee6b81de9acf10930b1a2f0aeae7296ffa368d3f
Jussi Jartamo <jussi.jartamo@gofore.com>
2019-11-19 10:52:15 +0200

    code cleanup

--- src/main/app/src/components/sivu/Heading.js
+++ src/main/app/src/components/sivu/Heading.js
@@ -23,13 +23,13 @@ const Heading = ({...props,level}) => {
     const value = props.children[0].props.value;
     switch (level) {
        case 1:
-           return <h1 className={classes.anchor}><a className={classes.anchorLink} name={value}>{value}</a></h1>;
+           return <h1 className={classes.anchor}><a className={classes.anchorLink} name={value}/>{value}</h1>
        case 2:
-           return <h2 className={classes.anchor}><a className={classes.anchorLink} name={value}>{value}</a></h2>;
+           return <h2 className={classes.anchor}><a className={classes.anchorLink} name={value}/>{value}</h2>
        case 3:
-           return <h3 className={classes.anchor}><a className={classes.anchorLink} name={value}>{value}</a></h3>;
+           return <h3 className={classes.anchor}><a className={classes.anchorLink} name={value}/>{value}</h3>
        default:
-           return <h4 className={classes.anchor}><a className={classes.anchorLink} name={value}>{value}</a></h4>
+           return <h4 className={classes.anchor}><a className={classes.anchorLink} name={value}/>{value}</h4>
     }
 };
 
