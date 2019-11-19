e2efb5c23233b95fb93fa8d969d77c5453c60ec9
Jussi Jartamo <jussi.jartamo@gofore.com>
2019-11-19 10:59:54 +0200

    youtubelinkfixed

--- src/main/app/src/components/sivu/Youtube.js
+++ src/main/app/src/components/sivu/Youtube.js
@@ -6,7 +6,8 @@ const Youtube = (props) => {
     return <iframe title={props.url}
                    width="560" height="315"
                    style={{display: "block", margin: "10px 0px"}}
-                   src={`https://www.youtube.com/embed/${url.query.v}`}
+                   src={`https://www.youtube.com/embed${
+                       (url.query.v && '/'+url.query.v) || url.pathname}`}
                    frameBorder="0"
                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                    allowFullScreen={true}/>;
