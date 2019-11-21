589d745c50ba5737f682871f271b9f522e55b9e3
Jussi Jartamo <jussi.jartamo@gofore.com>
2019-11-21 12:24:33 +0200

    better responsive images

--- src/main/app/src/components/sivu/Sisalto.js
+++ src/main/app/src/components/sivu/Sisalto.js
@@ -8,6 +8,7 @@ import Youtube from "./Youtube";
 import OpenInNewIcon from "@material-ui/core/SvgIcon/SvgIcon";
 import {colors} from "../../colors";
 import {makeStyles} from "@material-ui/core";
+import Grid from '@material-ui/core/Grid';
 import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
 
@@ -44,9 +45,8 @@ const useStyles = makeStyles({
         paddingTop: '56.25%',
     },
     card: {
-        maxWidth: '45vw',
-        transitionDuration: '0.3s',
-        maxHeight: '45vw'
+    },
+    imageContainer: {
     }
 });
 
@@ -57,7 +57,14 @@ const Sisalto = ({content, contentfulStore}) => {
 
     const ImageComponent = ({...props, src, alt}) => {
         const url = src.replace("//images.ctfassets.net/", "")
-        return<Card className={classes.card}
+        return <Grid container
+                     direction="row"
+                     justify="flex-start"
+                     alignItems="flex-start"
+                     className={classes.imageContainer}
+
+        ><Grid item xs={12} sm={6} md={6}>
+            <Card className={classes.card}
                     elevation={0}
         >
             <CardMedia
@@ -65,7 +72,8 @@ const Sisalto = ({content, contentfulStore}) => {
                 image={contentfulStore.assetUrl(url)}
                 title={alt}
             /></Card>
-
+        </Grid>
+        </Grid>
 
     };
     const SivuLink = props => {
