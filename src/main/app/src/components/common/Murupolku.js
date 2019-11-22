7eece9d0b1f5f1befec66055f6af725702a0288b
Jussi Jartamo <jussi.jartamo@gofore.com>
2019-11-22 14:39:57 +0200

    initial murupolku fixes

--- src/main/app/src/components/common/Murupolku.js
+++ src/main/app/src/components/common/Murupolku.js
@@ -5,12 +5,23 @@ import {makeStyles} from "@material-ui/core";
 import Icon from '@material-ui/core/Icon';
 import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
 import Link from '@material-ui/core/Link';
+import clsx from 'clsx';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import {colors} from "../../colors";
+import _ from 'lodash';
 
 const useStyles = makeStyles({
     breadcrump: {
+        height: "20px"
+    },
+    forwardIcon: {
+        color: colors.lightGrey,
+        marginRight: "10px",
+        marginLeft: "10px"
     },
     icon: {
         marginRight: "10px"
+
     },
     link: {
         fontWeight: "400",
@@ -19,7 +30,11 @@ const useStyles = makeStyles({
         overflow: "hidden",
         whiteSpace: "nowrap",
         textOverflow: "ellipsis",
-        paddingRight: "15px",
+        verticalAlign: "top",
+        color: colors.grey
+    },
+    lastLink: {
+        color: colors.green
     }
 });
 
@@ -29,15 +44,20 @@ const Murupolku = ({path, history}) => {
     const forwardToFrontPage = (path) => {
         history.push(path);
     };
+    const last = path[path.length - 1];
     return <div className={classes.breadcrump}>
         <Icon className={classes.icon}><HomeOutlinedIcon/></Icon>
-
         <Link onClick={() => forwardToFrontPage('/')} className={classes.link}>{t('etusivu')}</Link>
-        {path.map(({name, link}, ind) => {
+        {path.map(({...currentLink, name, link}, ind) => {
             return [
-                link ? <Link key={`breadcrumplink-${ind}`} className={classes.link}
-                                  to={link}>{name}</Link>
-                    : <span key={`breadcrumplink-${ind}`} className={classes.link}>{name}</span>
+                <ArrowForwardIosIcon style={{ fontSize: "12px", height: "20px" }}
+                                     key={`breadcrumpseparator-${ind}`}
+                                     className={classes.forwardIcon}/>,
+                link ? <Link key={`breadcrumplink-${ind}`}
+                             className={clsx(classes.link, _.isEqual(currentLink, last) && classes.lastLink)}
+                             to={link}>{name}</Link>
+                    : <span key={`breadcrumplink-${ind}`}
+                            className={clsx(classes.link)}>{name}</span>
             ];
         })}
     </div>;
