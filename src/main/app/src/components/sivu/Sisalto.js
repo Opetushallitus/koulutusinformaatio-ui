e089e16c6d654cfa1cd5a93781f8368569518a5c
Jussi Jartamo <jussi.jartamo@gofore.com>
2019-12-10 15:35:32 +0200

    p -> div so that images can be responsive (div) cards without html best practise violations

--- src/main/app/src/components/sivu/Sisalto.js
+++ src/main/app/src/components/sivu/Sisalto.js
@@ -10,6 +10,7 @@ import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import { withRouter } from 'react-router-dom';
+import { observer } from 'mobx-react';
 
 const useStyles = makeStyles({
   notFound: {
@@ -43,129 +44,125 @@ const useStyles = makeStyles({
   },
 });
 
-const Sisalto = ({
-  content,
-  contentfulStore,
-  alwaysFullWidth,
-  excludeMedia,
-  ...props
-}) => {
-  const classes = useStyles();
-  const { forwardTo } = contentfulStore;
-  const { sivu } = contentfulStore.data;
-  const forwardToPage = (e, id) => {
-    props.history.push(forwardTo(id));
-    e.preventDefault();
-  };
-  const Null = (props) => null;
-  const ImageComponent = ({ src, alt, ...props }) => {
-    const url = src.replace('//images.ctfassets.net/', '');
-    return (
-      <Grid
-        container
-        direction="row"
-        justify="flex-start"
-        alignItems="flex-start"
-        className={classes.imageContainer}
-      >
+const Sisalto = observer(
+  ({ content, contentfulStore, alwaysFullWidth, excludeMedia, ...props }) => {
+    const classes = useStyles();
+    const { forwardTo } = contentfulStore;
+    const { sivu } = contentfulStore.data;
+    const forwardToPage = (e, id) => {
+      props.history.push(forwardTo(id));
+      e.preventDefault();
+    };
+    const Null = (props) => null;
+    const ImageComponent = ({ src, alt, ...props }) => {
+      const url = src.replace('//images.ctfassets.net/', '');
+      return (
         <Grid
-          item
-          xs={12}
-          sm={alwaysFullWidth ? 12 : 12}
-          md={alwaysFullWidth ? 12 : 12}
+          container
+          direction="row"
+          justify="flex-start"
+          alignItems="flex-start"
+          className={classes.imageContainer}
         >
-          <Card className={classes.card} elevation={0}>
-            <CardMedia
-              className={classes.media}
-              image={contentfulStore.assetUrl(url)}
-              title={alt}
-            />
-          </Card>
+          <Grid
+            item
+            xs={12}
+            sm={alwaysFullWidth ? 12 : 12}
+            md={alwaysFullWidth ? 12 : 12}
+          >
+            <Card className={classes.card} elevation={0}>
+              <CardMedia
+                className={classes.media}
+                image={contentfulStore.assetUrl(url)}
+                title={alt}
+              />
+            </Card>
+          </Grid>
         </Grid>
-      </Grid>
+      );
+    };
+    const SivuLink = (props) => {
+      const id = props.children[0];
+      return <Link onClick={(e) => forwardToPage(e, id)}>{sivu[id].name}</Link>;
+    };
+    const LinkOrYoutube = ({ children, className, ...props }) => {
+      if (className === 'embedly-card') {
+        return (
+          <div className={classes.youtube}>
+            <Youtube {...props} />
+          </div>
+        );
+      } else {
+        return (
+          <Link target="_blank" rel="noopener" {...props}>
+            {children}
+            <OpenInNewIcon className={classes.icon} />
+          </Link>
+        );
+      }
+    };
+    return (
+      <Markdown
+        options={{
+          overrides: {
+            img: {
+              component: excludeMedia ? Null : ImageComponent,
+            },
+            h1: {
+              component: Typography,
+              props: {
+                variant: 'h1',
+                gutterBottom: true,
+              },
+            },
+            h2: {
+              component: Typography,
+              props: {
+                variant: 'h2',
+                gutterBottom: true,
+              },
+            },
+            h3: {
+              component: Typography,
+              props: {
+                variant: 'h3',
+                gutterBottom: true,
+              },
+            },
+            h4: {
+              component: Typography,
+              props: {
+                variant: 'h4',
+                gutterBottom: true,
+              },
+            },
+            p: {
+              component: Typography,
+              props: {
+                variant: 'body1',
+                component: 'div',
+                paragraph: true,
+              },
+            },
+            a: {
+              component: excludeMedia ? Null : LinkOrYoutube,
+            },
+            details: {
+              component: excludeMedia ? Null : Accordion,
+            },
+            summary: {
+              component: excludeMedia ? Null : Summary,
+            },
+            sivu: {
+              component: SivuLink,
+            },
+          },
+        }}
+      >
+        {content}
+      </Markdown>
     );
-  };
-  const SivuLink = (props) => {
-    const id = props.children[0];
-    return <Link onClick={(e) => forwardToPage(e, id)}>{sivu[id].name}</Link>;
-  };
-  const LinkOrYoutube = ({ children, className, ...props }) => {
-    if (className === 'embedly-card') {
-      return (
-        <div className={classes.youtube}>
-          <Youtube {...props} />
-        </div>
-      );
-    } else {
-      return (
-        <Link target="_blank" rel="noopener" {...props}>
-          {children}
-          <OpenInNewIcon className={classes.icon} />
-        </Link>
-      );
-    }
-  };
-  return (
-    <Markdown
-      options={{
-        overrides: {
-          img: {
-            component: excludeMedia ? Null : ImageComponent,
-          },
-          h1: {
-            component: Typography,
-            props: {
-              variant: 'h1',
-              gutterBottom: true,
-            },
-          },
-          h2: {
-            component: Typography,
-            props: {
-              variant: 'h2',
-              gutterBottom: true,
-            },
-          },
-          h3: {
-            component: Typography,
-            props: {
-              variant: 'h3',
-              gutterBottom: true,
-            },
-          },
-          h4: {
-            component: Typography,
-            props: {
-              variant: 'h4',
-              gutterBottom: true,
-            },
-          },
-          p: {
-            component: Typography,
-            props: {
-              variant: 'body1',
-              component: 'p',
-              paragraph: true,
-            },
-          },
-          a: {
-            component: excludeMedia ? Null : LinkOrYoutube,
-          },
-          details: {
-            component: excludeMedia ? Null : Accordion,
-          },
-          summary: {
-            component: excludeMedia ? Null : Summary,
-          },
-          sivu: {
-            component: SivuLink,
-          },
-        },
-      }}
-    >
-      {content}
-    </Markdown>
-  );
-};
+  }
+);
 
 export default withRouter(Sisalto);
