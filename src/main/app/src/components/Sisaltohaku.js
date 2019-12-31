b4291a9da988a0261c2611e664af4e89784162c2
Jussi Jartamo <jussi.jartamo@gofore.com>
2019-12-31 12:14:11 +0200

    code cleanup: obsolete useCallback removed

--- src/main/app/src/components/Sisaltohaku.js
+++ src/main/app/src/components/Sisaltohaku.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import Murupolku from './common/Murupolku';
 import Preview from './Preview';
