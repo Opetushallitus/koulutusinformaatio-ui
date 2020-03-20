c6a3b673175b00d9c13c002199dc9b95c7f508a8
Dmitry Marokhonov <dmitry.marokhonov@cybercom.com>
2020-03-20 13:50:24 +0200

    KTO-648: Cypress config added

--- src/main/app/cypress/plugins/index.js
+++ src/main/app/cypress/plugins/index.js
@@ -11,7 +11,20 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-module.exports = (on, config) => {
+const wp = require('@cypress/webpack-preprocessor');
+const alias = require('../../webpack-alias');
+
+module.exports = (on) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+  on(
+    'file:preprocessor',
+    wp({
+      webpackOptions: {
+        resolve: {
+          alias,
+        },
+      },
+    })
+  );
+};
