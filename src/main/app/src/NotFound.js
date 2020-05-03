69531e8c424fc32e0ff4e1954a2d3de33bfb67b0
Jussi Jartamo <jussi.jartamo@gofore.com>
2020-05-04 02:14:30 +0300

    styles updated

--- src/main/app/src/NotFound.js
+++ src/main/app/src/NotFound.js
@@ -2,18 +2,13 @@ import Grid from '@material-ui/core/Grid';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { makeStyles } from '@material-ui/core/styles';
-import { colors } from '#/src/colors';
 import { Box, Button, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles({
   component: {
     textAlign: 'center',
-    paddingLeft: '10px',
-    paddingRight: '10px',
     paddingTop: '132px',
     paddingBottom: '132px',
-    fontSize: '16px',
-    color: colors.grey,
   },
 });
 
@@ -21,43 +16,50 @@ const NotFound = () => {
   const { t } = useTranslation();
   const classes = useStyles();
   return (
-    <Grid
-      container
-      direction="row"
-      justify="center"
-      alignItems="center"
-      className={classes.component}>
-      <Grid item xs={12} sm={6} md={6} className={classes.notFound}>
-        <Box mt={1}>
+    <div className={classes.component}>
+      <Grid container direction="column" justify="center" alignItems="center" spacing={5}>
+        <Grid item>
           <Typography variant="h1" component="h1" color="secondary">
             {t('ei-loydy.404')}
           </Typography>
-        </Box>
-        <Box mt={1}>
+        </Grid>
+        <Grid item>
           <Typography variant="h1" component="h1">
             {t('ei-loydy.sivua-ei-löytynyt')}
           </Typography>
-        </Box>
-        <Box mt={1}>
           <Typography variant={'body1'} paragraph>
             {t('ei-loydy.linkki-on-virheellinen')}
           </Typography>
-        </Box>
-        <Button
-          variant="contained"
-          aria-label={t('ei-loydy.etusivulle')}
-          color="primary"
-          href={'/konfo'}>
-          {t('ei-loydy.etusivulle')}
-        </Button>
-        <Button
-          variant="contained"
-          aria-label={t('ei-loydy.takaisin')}
-          onClick={() => window.history.back()}>
-          {t('ei-loydy.takaisin')}
-        </Button>
+        </Grid>
+        <Grid item>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            spacing={2}>
+            <Grid item>
+              <Button
+                variant="contained"
+                aria-label={t('ei-loydy.etusivulle')}
+                color="primary"
+                href={'/konfo'}>
+                {t('ei-loydy.etusivulle')}
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="outlined"
+                color="primary"
+                aria-label={t('ei-loydy.takaisin')}
+                onClick={() => window.history.back()}>
+                {t('ei-loydy.takaisin')}
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
       </Grid>
-    </Grid>
+    </div>
   );
 };
 
