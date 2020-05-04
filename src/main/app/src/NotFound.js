dda6e07c6b82861c5b0076d19c6b06ecb5b64ddd
Jussi Jartamo <jussi.jartamo@gofore.com>
2020-05-04 13:23:42 +0300

    code cleanup

--- src/main/app/src/NotFound.js
+++ src/main/app/src/NotFound.js
@@ -1,8 +1,7 @@
-import Grid from '@material-ui/core/Grid';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, Button, Typography } from '@material-ui/core';
+import { Grid, Box, Button, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles({
   component: {
