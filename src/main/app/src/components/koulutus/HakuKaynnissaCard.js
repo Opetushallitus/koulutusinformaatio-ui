aaeaa86d0cf771f9a3fb87bfbfa18ceee02e267c
Jussi Jartamo <jussi.jartamo@gofore.com>
2020-07-08 09:46:59 +0300

    kto-784

--- src/main/app/src/components/koulutus/HakuKaynnissaCard.js
+++ src/main/app/src/components/koulutus/HakuKaynnissaCard.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { Paper, Typography, makeStyles, Grid, Button } from '@material-ui/core';
+import {
+  Card,
+  CardContent,
+  CardActions,
+  CardActionArea,
+  Typography,
+  makeStyles,
+  Grid,
+  Button,
+} from '@material-ui/core';
 
 const useStyles = makeStyles({
-  card: { width: '50%', height: '130px', marginTop: '100px' },
-  gridWrapper: { margin: '40px' },
+  card: { margin: '40px', padding: '20px' },
+  gridWrapper: {},
   buttonText: { color: '#FFFFFF' },
 });
 
@@ -12,29 +21,31 @@ const HakuKaynnissaCard = (props) => {
   const { title, text, link, buttonText } = props;
   return (
     <>
-      <Paper className={classes.card} elevation={2}>
-        <div className={classes.gridWrapper}>
-          <Grid container alignItems="center" justify="space-between">
-            <Grid item>
-              <Typography variant="h4" gutterBottom>
-                {title}
-              </Typography>
-              <Typography variant="body1">{text}</Typography>
+      <Card className={classes.card} elevation={2}>
+        <CardActionArea>
+          <CardContent className={classes.gridWrapper}>
+            <Grid container alignItems="center" justify="space-between">
+              <Grid item>
+                <Typography variant="h4" gutterBottom>
+                  {title}
+                </Typography>
+                <Typography variant="body1">{text}</Typography>
+              </Grid>
             </Grid>
-            <Grid item>
-              {React.cloneElement(link, {
-                children: (
-                  <Button variant="contained" size="large" color="primary">
-                    <Typography className={classes.buttonText} variant="body1">
-                      {buttonText}
-                    </Typography>
-                  </Button>
-                ),
-              })}
-            </Grid>
-          </Grid>
-        </div>
-      </Paper>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          {React.cloneElement(link, {
+            children: (
+              <Button variant="contained" size="large" color="primary">
+                <Typography className={classes.buttonText} variant="body1">
+                  {buttonText}
+                </Typography>
+              </Button>
+            ),
+          })}
+        </CardActions>
+      </Card>
     </>
   );
 };
