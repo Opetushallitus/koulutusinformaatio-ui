40712fe45075da3f0c08b438d31294990cb45ff1
Mikael Harju <mikael.harju@gofore.com>
2020-08-19 16:28:13 +0300

    Add custom pictures to oppilaitos osa

--- src/main/app/src/components/common/ImageCardGrid.js
+++ src/main/app/src/components/common/ImageCardGrid.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ImageCardWithText from '#/src/components/common/ImageCardWithText';
 import { Grid, useTheme, useMediaQuery } from '@material-ui/core';
-import DefaultHeroImage from '#/src/assets/images/herokuva_default.png';
-
 const OppilaitosOsaGrid = (props) => {
   const { cards } = props;
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-  console.log(cards);
   return (
     <Grid justify="center" container spacing={isMobile ? 1 : 3}>
-      {cards.map((osa, i) => (
+      {cards.map((card, i) => (
         <Grid item key={`ImageCardWithText-${i}`}>
-          <ImageCardWithText image={DefaultHeroImage} cardText={osa.nimi} />
+          <ImageCardWithText image={card.image} cardText={card.text} />
         </Grid>
       ))}
     </Grid>
--- src/main/app/src/components/oppilaitos/OppilaitosOsaList.js
+++ src/main/app/src/components/oppilaitos/OppilaitosOsaList.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ImageCardGrid from '#/src/components/common/ImageCardGrid';
 import { Box, Typography, useTheme, useMediaQuery } from '@material-ui/core';
 import Spacer from '../common/Spacer';
+import DefaultHeroImage from '#/src/assets/images/herokuva_default.png';
 
 const OppilaitosOsaList = (props) => {
   const { oppilaitosOsat, title } = props;
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const cardInfos = oppilaitosOsat.map((osa) => ({
+    text: osa.nimi,
+    image: osa?.oppilaitoksenOsa?.teemakuva || DefaultHeroImage,
+  }));
   return (
     <Box mt={isMobile ? 6 : 12} display="flex" alignItems="center" flexDirection="column">
       <Typography variant="h2">{title}</Typography>
       <Spacer />
       <Box mt={isMobile ? 0 : 2}>
-        <ImageCardGrid cards={oppilaitosOsat} />
+        <ImageCardGrid cards={cardInfos} />
       </Box>
     </Box>
   );
