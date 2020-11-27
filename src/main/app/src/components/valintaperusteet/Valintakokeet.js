846a7458da7b95011b83d51ae893de264b93c37d
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-11-27 09:17:49 +0200

    KTO-1007: Improve valintaperuste valintakokeet
    - Add missing fields (valmistatumisohjeet, ohjeet erityisjärjestelyihin)
    - Make valintakokeet box look nicer
    - Put each tilaisuus in an Accordion

--- src/main/app/src/components/valintaperusteet/Valintakokeet.js
+++ src/main/app/src/components/valintaperusteet/Valintakokeet.js
@@ -1,21 +1,42 @@
 import React from 'react';
+import _ from 'lodash';
 import { Box, Card, CardContent, Grid, Typography } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
-import _ from 'lodash';
-import { formatDateString, Localizer as l, Parser as p } from '#/src/tools/Utils';
+import { formatDateString, Localizer as l, sanitizedHTMLParser } from '#/src/tools/Utils';
 import { colors } from '#/src/colors';
 import Spacer from '#/src/components/common/Spacer';
 import hyphenated from '#/src/components/valintaperusteet/hyphenated';
+import Accordion from '#/src/components/common/Accordion';
 
-const Osoite = ({ alkaa, paattyy, lisatietoja, osoite, postinumero }) => {
+const renderLocalizedHTML = (transObj) => sanitizedHTMLParser(l.localize(transObj));
+
+const SubHeading = ({ children }) => (
+  <Typography variant="h5" style={{ fontWeight: 700, color: colors.grey }}>
+    {children}
+  </Typography>
+);
+
+const Tilaisuus = ({ alkaa, paattyy, lisatietoja, osoite, postinumero }) => {
   const { t } = useTranslation();
   return (
     <>
+      <Grid item xs={6}>
+        <Box py={1}>
+          <SubHeading>{t('valintaperuste.alkaa')}</SubHeading>
+          <Typography variant="body1">{formatDateString(alkaa)}</Typography>
+        </Box>
+      </Grid>
+      <Grid item xs={6}>
+        <Box py={1}>
+          <SubHeading>{t('valintaperuste.paattyy')}</SubHeading>
+          <Typography variant="body1">{formatDateString(paattyy)}</Typography>
+        </Box>
+      </Grid>
       {postinumero ? (
         <>
           <Grid item xs={12}>
             <Box py={1}>
-              <Typography variant="h5">{t('valintaperuste.osoite')}</Typography>
+              <SubHeading>{t('valintaperuste.osoite')}</SubHeading>
               <Typography variant="body1">
                 {l.localizeOsoite(osoite, postinumero)}
               </Typography>
@@ -23,26 +44,12 @@ const Osoite = ({ alkaa, paattyy, lisatietoja, osoite, postinumero }) => {
           </Grid>
         </>
       ) : null}
-      <Grid item xs={6}>
-        <Box py={1}>
-          <Typography variant="h5">{t('valintaperuste.alkaa')}</Typography>
-          <Typography variant="body1">{formatDateString(alkaa)}</Typography>
-        </Box>
-      </Grid>
-      <Grid item xs={6}>
-        <Box py={1}>
-          <Typography variant="h5">{t('valintaperuste.paattyy')}</Typography>
-          <Typography variant="body1">{formatDateString(paattyy)}</Typography>
-        </Box>
-      </Grid>
       {!_.isEmpty(lisatietoja) ? (
         <>
           <Grid item xs={12}>
             <Box py={1}>
-              <Typography variant="h5">{t('valintaperuste.lisatietoja')}</Typography>
-            </Box>
-            <Box py={1}>
-              <Typography variant="body1">{l.localize(lisatietoja)}</Typography>
+              <SubHeading>{t('valintaperuste.lisatietoja')}</SubHeading>
+              {renderLocalizedHTML(lisatietoja)}
             </Box>
           </Grid>
         </>
@@ -67,46 +74,73 @@ export const Valintakokeet = ({ valintakokeet }) => {
         <Typography variant="h2">{t('valintaperuste.valintakokeet')}</Typography>
         <Spacer />
       </Box>
-      {valintakokeet.map(({ tyyppi: { nimi }, tilaisuudet }, index) => {
+      {valintakokeet.map(({ nimi, tyyppi, tilaisuudet, metadata = {} }, index) => {
+        const localizedTyyppi = l.localize(tyyppi?.nimi);
+        const {
+          ohjeetErityisjarjestelyihin,
+          ohjeetEnnakkovalmistautumiseen,
+          tietoja,
+        } = metadata;
         return (
           <div key={`valintakoe-${index}`}>
             <Card
-              elevation={1}
+              elevation={0}
               style={{
                 backgroundColor: colors.veryLightGrey,
                 padding: '15px',
                 marginBottom: '20px',
               }}>
               <CardContent>
-                <Box py={2}>
-                  <Typography
-                    id={`${hyphenated(l.localize(nimi))}-${index + 1}`}
-                    variant="h4">
-                    {l.localize(nimi)}
-                  </Typography>
-                </Box>
-                {tilaisuudet.map(
-                  (
-                    {
-                      lisatietoja,
-                      osoite: { osoite, postinumero },
-                      aika: { alkaa, paattyy },
-                    },
-                    index
-                  ) => {
-                    return (
-                      <Grid container key={`koetilaisuus-${index}`}>
-                        <Osoite
-                          osoite={osoite}
-                          lisatietoja={lisatietoja}
-                          postinumero={postinumero}
-                          alkaa={alkaa}
-                          paattyy={paattyy}
-                        />
-                      </Grid>
-                    );
-                  }
+                <Typography
+                  id={`${hyphenated(localizedTyyppi)}-${index + 1}`}
+                  variant="body1">
+                  {localizedTyyppi}
+                </Typography>
+                <SubHeading>{l.localize(nimi)}</SubHeading>
+                {renderLocalizedHTML(tietoja)}
+                {!_.isEmpty(ohjeetEnnakkovalmistautumiseen) && (
+                  <>
+                    <SubHeading>
+                      {t('valintaperuste.valmistautumisohjeet-hakijalle')}
+                    </SubHeading>
+                    {renderLocalizedHTML(ohjeetEnnakkovalmistautumiseen)}
+                  </>
                 )}
+                {!_.isEmpty(ohjeetErityisjarjestelyihin) && (
+                  <>
+                    <SubHeading>
+                      {t('valintaperuste.ohjeet-erityisjarjestelyihin')}
+                    </SubHeading>
+                    {renderLocalizedHTML(ohjeetErityisjarjestelyihin) || '-'}
+                  </>
+                )}
+                <Accordion
+                  items={tilaisuudet.map(
+                    (
+                      {
+                        lisatietoja,
+                        osoite: { osoite, postinumero },
+                        aika: { alkaa, paattyy },
+                      },
+                      index
+                    ) => {
+                      return {
+                        title: `${t('valintaperuste.tilaisuus')} ${index + 1}`,
+                        content: (
+                          <Grid container key={`koetilaisuus-${index}`}>
+                            <Tilaisuus
+                              osoite={osoite}
+                              lisatietoja={lisatietoja}
+                              postinumero={postinumero}
+                              alkaa={alkaa}
+                              paattyy={paattyy}
+                            />
+                          </Grid>
+                        ),
+                      };
+                    }
+                  )}
+                />
               </CardContent>
             </Card>
           </div>
