821aed02a722ddd0be6355a4c818265460645e67
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-11-27 14:34:59 +0200

    KTO-823: Fix murupoku cypress test urls (remove /konfo)

--- src/main/app/cypress/integration/murupolku.spec.js
+++ src/main/app/cypress/integration/murupolku.spec.js
@@ -5,7 +5,7 @@ describe('Murupolku', function () {
   autoRecord();
 
   it('Should show correct breadcrumb for a contentful page', function () {
-    const url = '/konfo/fi/sivu/paikan-vastaanotto-ja-ilmoittautuminen-korkeakouluun';
+    const url = '/fi/sivu/paikan-vastaanotto-ja-ilmoittautuminen-korkeakouluun';
     cy.visit(url);
 
     assertBreadcrumb({
@@ -17,7 +17,7 @@ describe('Murupolku', function () {
   });
 
   it('Should show correct breadcrumb for koulutus', function () {
-    const url = '/konfo/fi/koulutus/1.2.246.562.13.00000000000000000570';
+    const url = '/fi/koulutus/1.2.246.562.13.00000000000000000570';
     cy.visit(url);
 
     assertBreadcrumb({
@@ -29,7 +29,7 @@ describe('Murupolku', function () {
   });
 
   it('Should show correct breadcrumb for toteutus', function () {
-    const url = '/konfo/fi/toteutus/1.2.246.562.17.00000000000000000404';
+    const url = '/fi/toteutus/1.2.246.562.17.00000000000000000404';
     cy.visit(url);
 
     assertBreadcrumb({
@@ -41,7 +41,7 @@ describe('Murupolku', function () {
   });
 
   it('Should show correct breadcrumb for oppilaitos', function () {
-    const url = '/konfo/fi/oppilaitos/1.2.246.562.10.56753942459';
+    const url = '/fi/oppilaitos/1.2.246.562.10.56753942459';
     cy.visit(url);
 
     assertBreadcrumb({
@@ -53,7 +53,7 @@ describe('Murupolku', function () {
   });
 
   it('Should show correct breadcrumb for oppilaitoksen osa', function () {
-    const url = '/konfo/fi/oppilaitososa/1.2.246.562.10.61042218794';
+    const url = '/fi/oppilaitososa/1.2.246.562.10.61042218794';
     cy.visit(url);
 
     assertBreadcrumb({
@@ -66,7 +66,7 @@ describe('Murupolku', function () {
 
   it('Should show correct breadcrumb for valintaperuste', function () {
     const url =
-      '/konfo/fi/hakukohde/1.2.246.562.20.00000000000000000429/valintaperuste/4a1037c9-290c-48c1-a726-9833e1b2d749';
+      '/fi/hakukohde/1.2.246.562.20.00000000000000000429/valintaperuste/4a1037c9-290c-48c1-a726-9833e1b2d749';
     cy.visit(url);
 
     assertBreadcrumb({
