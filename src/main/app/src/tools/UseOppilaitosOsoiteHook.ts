0230e5defff2cd6a684519aa698f39a5bf42497e
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2020-11-30 14:58:50 +0200

    KTO-1005: Lisätty hookin palautusarvoon oppilaitoksen oid ja tyypitetty palautusarvo

--- src/main/app/src/tools/UseOppilaitosOsoiteHook.ts
+++ src/main/app/src/tools/UseOppilaitosOsoiteHook.ts
@@ -2,26 +2,36 @@ import { getOppilaitosOsa } from '#/src/api/konfoApi';
 import { OsoiteParser } from '#/src/tools/Utils';
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
+export interface OppilaitosOsoite {
+  oppilaitosOid: string;
+  yhteystiedot: any;
+}
+
 async function fetchOsoitteet(
   oppilaitosOids: String[],
-  setOsoitteet: Dispatch<SetStateAction<any[]>>
+  setOsoitteet: Dispatch<SetStateAction<OppilaitosOsoite[]>>
 ) {
   if (oppilaitosOids.length > 0) {
     const oppilaitosDatas = await Promise.all(
       oppilaitosOids.map((oid) => getOppilaitosOsa(oid))
     );
-    const osoitteet = oppilaitosDatas.map((data) => {
+
+    const osoitteet: OppilaitosOsoite[] = oppilaitosDatas.map((data) => {
       // NOTE: Prioritize using oppilaitoksenOsa, otherwise the address of main oppilaitos is found inside oppilaitos-property
       const osoiteData = (data?.oppilaitoksenOsa || data?.oppilaitos?.oppilaitos).metadata
         .yhteystiedot.osoite;
-      return OsoiteParser.parseOsoiteData(osoiteData).yhteystiedot;
+      return {
+        oppilaitosOid: data.oid,
+        yhteystiedot: OsoiteParser.parseOsoiteData(osoiteData).yhteystiedot,
+      };
     });
+
     setOsoitteet(osoitteet);
   }
 }
 
 export const useOppilaitosOsoite = (oppilaitosOids: String[]) => {
-  const [osoitteet, setOsoitteet] = useState([] as any[]);
+  const [osoitteet, setOsoitteet] = useState([] as OppilaitosOsoite[]);
   const [dataNotFetched, setDataNotFetched] = useState(true);
 
   useEffect(() => {
