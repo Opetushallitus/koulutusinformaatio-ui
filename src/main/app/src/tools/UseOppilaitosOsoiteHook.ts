1fb19d71a9a51044f638b06e48f4a1253139ed8a
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2020-12-01 09:24:50 +0200

    KTO-1005: Poistettu tarpeeton if

--- src/main/app/src/tools/UseOppilaitosOsoiteHook.ts
+++ src/main/app/src/tools/UseOppilaitosOsoiteHook.ts
@@ -11,23 +11,21 @@ async function fetchOsoitteet(
   oppilaitosOids: String[],
   setOsoitteet: Dispatch<SetStateAction<OppilaitosOsoite[]>>
 ) {
-  if (oppilaitosOids.length > 0) {
-    const oppilaitosDatas = await Promise.all(
-      oppilaitosOids.filter(Boolean).map((oid) => getOppilaitosOsa(oid))
-    );
+  const oppilaitosDatas = await Promise.all(
+    oppilaitosOids.filter(Boolean).map((oid) => getOppilaitosOsa(oid))
+  );
 
-    const osoitteet: OppilaitosOsoite[] = oppilaitosDatas.map((data) => {
-      // NOTE: Prioritize using oppilaitoksenOsa, otherwise the address of main oppilaitos is found inside oppilaitos-property
-      const osoiteData = (data?.oppilaitoksenOsa || data?.oppilaitos?.oppilaitos).metadata
-        .yhteystiedot.osoite;
-      return {
-        oppilaitosOid: data.oid,
-        yhteystiedot: OsoiteParser.parseOsoiteData(osoiteData).yhteystiedot,
-      };
-    });
+  const osoitteet: OppilaitosOsoite[] = oppilaitosDatas.map((data) => {
+    // NOTE: Prioritize using oppilaitoksenOsa, otherwise the address of main oppilaitos is found inside oppilaitos-property
+    const osoiteData = (data?.oppilaitoksenOsa || data?.oppilaitos?.oppilaitos).metadata
+      .yhteystiedot.osoite;
+    return {
+      oppilaitosOid: data.oid,
+      yhteystiedot: OsoiteParser.parseOsoiteData(osoiteData).yhteystiedot,
+    };
+  });
 
-    setOsoitteet(osoitteet);
-  }
+  setOsoitteet(osoitteet);
 }
 
 export const useOppilaitosOsoite = (oppilaitosOids: String[]) => {
