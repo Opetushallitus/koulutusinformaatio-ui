9b130a79270287bbb7b2ba22a857bc5b02e76409
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2020-12-01 12:44:58 +0200

    KTO-1005: Siirretty setOsoiteet datan hakufunktion sisältä pois

--- src/main/app/src/tools/UseOppilaitosOsoiteHook.ts
+++ src/main/app/src/tools/UseOppilaitosOsoiteHook.ts
@@ -1,21 +1,18 @@
 import { getOppilaitosOsa } from '#/src/api/konfoApi';
 import { OsoiteParser } from '#/src/tools/Utils';
-import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export interface OppilaitosOsoite {
   oppilaitosOid: string;
   yhteystiedot: string;
 }
 
-async function fetchOsoitteet(
-  oppilaitosOids: String[],
-  setOsoitteet: Dispatch<SetStateAction<OppilaitosOsoite[]>>
-) {
+async function fetchOsoitteet(oppilaitosOids: String[]) {
   const oppilaitosDatas = await Promise.all(
     oppilaitosOids.filter(Boolean).map((oid) => getOppilaitosOsa(oid))
   );
 
-  const osoitteet: OppilaitosOsoite[] = oppilaitosDatas.map((data) => {
+  return oppilaitosDatas.map((data) => {
     // NOTE: Prioritize using oppilaitoksenOsa, otherwise the address of main oppilaitos is found inside oppilaitos-property
     const osoiteData = (data?.oppilaitoksenOsa || data?.oppilaitos?.oppilaitos).metadata
       .yhteystiedot.osoite;
@@ -24,19 +21,18 @@ async function fetchOsoitteet(
       yhteystiedot: OsoiteParser.parseOsoiteData(osoiteData).yhteystiedot,
     };
   });
-
-  setOsoitteet(osoitteet);
 }
 
 export const useOppilaitosOsoite = (oppilaitosOids: String[]) => {
-  const [osoitteet, setOsoitteet] = useState([] as OppilaitosOsoite[]);
+  const [osoitteet, setOsoitteet] = useState<OppilaitosOsoite[]>();
   const [dataNotFetched, setDataNotFetched] = useState(true);
 
   useEffect(() => {
     if (dataNotFetched) {
       (async () => {
         try {
-          await fetchOsoitteet(oppilaitosOids, setOsoitteet);
+          const data = await fetchOsoitteet(oppilaitosOids);
+          setOsoitteet(data);
         } catch (e) {
           console.error(e);
         } finally {
