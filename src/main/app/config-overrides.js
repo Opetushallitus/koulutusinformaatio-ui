02fd98cb3daca265d2bb85df4b3d970b190ee986
Antti Sarjanoja <antti.sarjanoja@gofore.com>
2020-12-28 15:59:57 +0200

    fix cypress test

--- src/main/app/config-overrides.js
+++ src/main/app/config-overrides.js
@@ -3,6 +3,8 @@ const { override, useBabelRc, addWebpackAlias } = require('customize-cra');
 
 const { CI } = process.env;
 
+// NOTE: it seems that codacy doesn't check local .eslintignore -file
+// eslint-disable-next-line
 module.exports = override(useBabelRc(), addWebpackAlias(alias), (config) => ({
   ...config,
   ...(CI
