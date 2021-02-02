38878b94723e56de1f5be02b7dcb00cf64fdb3c5
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-02-02 11:47:06 +0200

    Show loading circle for oppilaitos tarjonta lists
    - Add OverlayLoadingCircle for showing loading circle for individual components when old data is available
    - Show loading circles in oppilaitos tarjonta lists

--- src/main/app/src/components/oppilaitos/TarjontaList.js
+++ src/main/app/src/components/oppilaitos/TarjontaList.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Typography, Grid, Container, makeStyles } from '@material-ui/core';
 import { Link as RouterLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import _ from 'lodash';
 import Spacer from '#/src/components/common/Spacer';
 import { ToteutusCard } from '#/src/components/common/ToteutusCard';
 import TarjontaPagination from './TarjontaPagination';
 import { LocalizedLink } from '#/src/components/common/LocalizedLink';
 import { usePaginatedTarjonta } from './hooks';
+import { LoadingCircle, OverlayLoadingCircle } from '../common/LoadingCircle';
 
 const useStyles = makeStyles({
   container: {
@@ -30,41 +30,48 @@ const TarjontaList = ({ oid, isOppilaitosOsa }) => {
     isTuleva: false,
   });
 
-  const { data: tarjonta = {}, isLoading } = queryResult;
+  const { data: tarjonta = {}, status, isFetching } = queryResult;
+  const { values, total } = tarjonta;
 
-  return !isLoading && tarjonta?.hasHits ? (
-    <Container maxWidth="lg" className={classes.container}>
-      <Typography variant="h2">
-        {t('oppilaitos.oppilaitoksessa-jarjestettavat-koulutukset')}
-      </Typography>
-      <Spacer />
-      <Grid container className={classes.grid} direction="column" spacing={1}>
-        {_.map(tarjonta?.values, (toteutus) => (
-          <Grid item key={toteutus?.toteutusOid}>
-            <LocalizedLink
-              underline="none"
-              component={RouterLink}
-              to={`/toteutus/${toteutus?.toteutusOid}`}>
-              <ToteutusCard
-                heading={toteutus?.toteutusName}
-                description={toteutus?.description}
-                locations={toteutus?.locations}
-                opetustapa={toteutus?.opetustapa}
-                price={toteutus?.price}
-                tyyppi={toteutus?.tyyppi}
-                image={toteutus?.kuva}
-              />
-            </LocalizedLink>
-          </Grid>
-        ))}
-      </Grid>
-      <TarjontaPagination
-        total={_.get(tarjonta, 'total')}
-        oid={oid}
-        isOppilaitosOsa={isOppilaitosOsa}
-      />
-    </Container>
-  ) : null;
+  switch (status) {
+    case 'loading':
+      return <LoadingCircle />;
+    case 'success':
+      return tarjonta.hasHits ? (
+        <Container maxWidth="lg" className={classes.container}>
+          <Typography variant="h2">
+            {t('oppilaitos.oppilaitoksessa-jarjestettavat-koulutukset')}
+          </Typography>
+          <Spacer />
+          <div style={{ position: 'relative' }}>
+            <OverlayLoadingCircle isLoading={isFetching} />
+            <Grid container className={classes.grid} direction="column" spacing={1}>
+              {values?.map((toteutus) => (
+                <Grid item key={toteutus?.toteutusOid}>
+                  <LocalizedLink
+                    underline="none"
+                    component={RouterLink}
+                    to={`/toteutus/${toteutus?.toteutusOid}`}>
+                    <ToteutusCard
+                      heading={toteutus?.toteutusName}
+                      description={toteutus?.description}
+                      locations={toteutus?.locations}
+                      opetustapa={toteutus?.opetustapa}
+                      price={toteutus?.price}
+                      tyyppi={toteutus?.tyyppi}
+                      image={toteutus?.kuva}
+                    />
+                  </LocalizedLink>
+                </Grid>
+              ))}
+            </Grid>
+          </div>
+          <TarjontaPagination total={total} oid={oid} isOppilaitosOsa={isOppilaitosOsa} />
+        </Container>
+      ) : null;
+    default:
+      return null;
+  }
 };
 
 export default TarjontaList;
--- src/main/app/src/components/oppilaitos/TulevaTarjontaList.tsx
+++ src/main/app/src/components/oppilaitos/TulevaTarjontaList.tsx
@@ -8,6 +8,7 @@ import { TulevaKoulutusCard } from './TulevaKoulutusCard';
 import { TulevaTarjontaPagination } from './TulevaTarjontaPagination';
 import { educationTypeColorCode } from '#/src/colors';
 import { usePaginatedTarjonta } from './hooks';
+import { LoadingCircle, OverlayLoadingCircle } from '../common/LoadingCircle';
 
 const useStyles = makeStyles({
   container: {
@@ -42,39 +43,54 @@ export const TulevaTarjontaList = ({ oid, isOppilaitosOsa }: Props) => {
     isTuleva: true,
   });
 
-  const { data: tulevaTarjonta = {} } = queryResult;
+  const { data: tulevaTarjonta = {}, isFetching, status } = queryResult;
   const { values, total } = tulevaTarjonta as {
     values: Array<Tarjonta>;
     total: number;
   };
 
-  return values ? (
-    <Container maxWidth="lg" className={classes.container}>
-      <Typography variant="h2">{t('oppilaitos.tulevat-koulutukset')}</Typography>
-      <Spacer />
-      <Grid container direction="row" justify="center" alignItems="stretch" spacing={1}>
-        {values.map((kts) => (
-          <Grid item key={kts.koulutusOid} xs={12} md={4}>
-            <LocalizedLink
-              underline="none"
-              component={RouterLink}
-              to={`/koulutus/${kts.koulutusOid}`}>
-              <TulevaKoulutusCard
-                koulutusName={kts.koulutusName}
-                tutkintonimikkeet={kts.tutkintonimikkeet}
-                koulutustyypit={kts.koulutustyypit}
-                opintojenlaajuus={kts.opintojenlaajuus}
-                tyyppi={kts.tyyppi}
-              />
-            </LocalizedLink>
-          </Grid>
-        ))}
-      </Grid>
-      <TulevaTarjontaPagination
-        total={total}
-        oid={oid}
-        isOppilaitosOsa={isOppilaitosOsa}
-      />
-    </Container>
-  ) : null;
+  switch (status) {
+    case 'loading':
+      return <LoadingCircle />;
+    case 'success':
+      return values ? (
+        <Container maxWidth="lg" className={classes.container}>
+          <Typography variant="h2">{t('oppilaitos.tulevat-koulutukset')}</Typography>
+          <Spacer />
+          <div style={{ position: 'relative' }}>
+            <OverlayLoadingCircle isLoading={isFetching} />
+            <Grid
+              container
+              direction="row"
+              justify="center"
+              alignItems="stretch"
+              spacing={1}>
+              {values.map((kts) => (
+                <Grid item key={kts.koulutusOid} xs={12} md={4}>
+                  <LocalizedLink
+                    underline="none"
+                    component={RouterLink}
+                    to={`/koulutus/${kts.koulutusOid}`}>
+                    <TulevaKoulutusCard
+                      koulutusName={kts.koulutusName}
+                      tutkintonimikkeet={kts.tutkintonimikkeet}
+                      koulutustyypit={kts.koulutustyypit}
+                      opintojenlaajuus={kts.opintojenlaajuus}
+                      tyyppi={kts.tyyppi}
+                    />
+                  </LocalizedLink>
+                </Grid>
+              ))}
+            </Grid>
+          </div>
+          <TulevaTarjontaPagination
+            total={total}
+            oid={oid}
+            isOppilaitosOsa={isOppilaitosOsa}
+          />
+        </Container>
+      ) : null;
+    default:
+      return null;
+  }
 };
