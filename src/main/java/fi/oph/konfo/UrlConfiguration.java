f0db29e3595d054eaabbb67cbbdd581a1cb7b8ff
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-05-20 12:45:37 +0300

    KTO-1235: Siirretty tuotannon URL konfiguraatiot spring boot wrapperiin
    - Linkki vanhaan opintopolkuun ylikirjoitettiin urls.ts-tiedostossa, jolloin käytettiin lokaalin kehityksen arvoa "/".

--- src/main/java/fi/oph/konfo/UrlConfiguration.java
+++ src/main/java/fi/oph/konfo/UrlConfiguration.java
@@ -18,16 +18,29 @@ public class UrlConfiguration extends OphProperties {
         this.addOverride("konfo-bucket", environment.getRequiredProperty("bucket.url"));
         this.addOverride("eperusteet-service.base-url", environment.getRequiredProperty("host.host-eperusteet-service"));
 
-        this.frontProperties.setProperty("eperusteet-service.eperuste.kuvaus", this.require("eperusteet-service.eperuste.kuvaus"));
         this.frontProperties.setProperty("konfo-backend.base-url", this.require("konfo-backend.base-url"));
         this.frontProperties.setProperty("konfo-backend.old-oppija", this.require("konfo-backend.old-oppija"));
         this.frontProperties.setProperty("konfo-backend.search.koulutukset", this.require("konfo-backend.search.koulutukset"));
-        this.frontProperties.setProperty("konfo-backend.koulutus", this.require("konfo-backend.koulutus"));
         this.frontProperties.setProperty("konfo-backend.search.oppilaitokset", this.require("konfo-backend.search.oppilaitokset"));
+        this.frontProperties.setProperty("konfo-backend.koulutus", this.require("konfo-backend.koulutus"));
+        this.frontProperties.setProperty("konfo-backend.suosittelu", this.require("konfo-backend.suosittelu"));
+        this.frontProperties.setProperty("konfo-backend.toteutus", this.require("konfo-backend.toteutus"));
+        this.frontProperties.setProperty("konfo-backend.valintaperusteet", this.require("konfo-backend.valintaperusteet"));
         this.frontProperties.setProperty("konfo-backend.oppilaitos", this.require("konfo-backend.oppilaitos"));
+        this.frontProperties.setProperty("konfo-backend.oppilaitosOsa", this.require("konfo-backend.oppilaitosOsa"));
+        this.frontProperties.setProperty("konfo-backend.kuvaus.osaamisalat", this.require("konfo-backend.kuvaus.osaamisalat"));
+
         this.frontProperties.setProperty("konfo-backend.palaute", this.require("konfo-backend.palaute"));
+        this.frontProperties.setProperty("konfo-backend.koulutus.jarjestajat", this.require("konfo-backend.koulutus.jarjestajat"));
+        this.frontProperties.setProperty("konfo-backend.oppilaitos.tarjonta", this.require("konfo-backend.oppilaitos.tarjonta"));
+        this.frontProperties.setProperty("konfo-backend.oppilaitosOsa.tarjonta", this.require("konfo-backend.oppilaitosOsa.tarjonta"));
+        this.frontProperties.setProperty("konfo-backend.hakukohde", this.require("konfo-backend.hakukohde"));
+        this.frontProperties.setProperty("konfo-backend.haku", this.require("konfo-backend.haku"));
+        this.frontProperties.setProperty("konfo-backend.koulutus.kuvaus", this.require("konfo-backend.koulutus.kuvaus"));
+
         this.frontProperties.setProperty("konfo-backend.content", this.require("konfo-backend.content"));
         this.frontProperties.setProperty("kartta.base-url", this.require("kartta.base-url"));
         this.frontProperties.setProperty("kartta.publish-url", this.require("kartta.publish-url"));
+        this.frontProperties.setProperty("eperusteet-service.eperuste.kuvaus", this.require("eperusteet-service.eperuste.kuvaus"));
     }
 }
