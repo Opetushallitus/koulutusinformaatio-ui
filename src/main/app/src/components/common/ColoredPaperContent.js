99d5c00be12c9cd732e195300e8921da99f99a94
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-06-28 09:47:22 +0300

    Eriytetty ColoredPaperContent-komponentti

---
+++ src/main/app/src/components/common/ColoredPaperContent.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Box, makeStyles, Paper } from '@material-ui/core';
+
+import { educationTypeColorCode } from '#/src/colors';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    width: '80%',
+    backgroundColor: ({ backgroundColor }) =>
+      backgroundColor ?? educationTypeColorCode.ammatillinenGreenBg,
+    [theme.breakpoints.down('sm')]: {
+      width: '95%',
+    },
+  },
+}));
+
+// Taustavärillinen "laatikko" koulutus, toteutus jne. sivuilla
+export const ColoredPaperContent = ({ children, ...props }) => {
+  const classes = useStyles(props);
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center" width="100%">
+      <Paper className={classes.paper}>{children}</Paper>
+    </Box>
+  );
+};
